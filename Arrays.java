1597df9bd7ee5a3863eb0dcacbb82e04f065f4f4
root <root@example.com>
2016-12-30 21:35:54 +0000

    new addition to our repository

--- Arrays.java
+++ Arrays.java
@@ -5,7 +5,7 @@ public class Arrays {
 	public static void main(String[] args) {
 		int[] myintarray = {1,2,3,4,5};
 		
-		/*
+		/*just practice
 		int[] myintarray = {1,3,5,7,9};
 		int myintarray2[] = {1,2,3,4};
 		int[] myintarray = new int[]{1,3,4,5,};
